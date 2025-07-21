eb0cfe65261628964fa605a74fd3e0e9ce4ca461
Samir Azam <azamsamir190@gmail.com>
2025-07-21 12:28:34 +0530

    feat: created complete backend routes including user, friend requests, and recommendation features

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { protectRoute } from '../middlewares/auth.middleware.js'
-import {getRecommendedUsers,getMyFriends, sendFriendRequest} from '../controllers/user.controller.js'
+import {getRecommendedUsers,getMyFriends, sendFriendRequest, acceptFriendRequest, getFriendRequests, getOutgoingFriendReqs} from '../controllers/user.controller.js'
 
 const router = express.Router()
 
@@ -8,6 +8,13 @@ router.use(protectRoute)
 
 router.get("/",getRecommendedUsers)
 router.get("/friends",getMyFriends)
-router.get("/friend-request/:id",sendFriendRequest)
+
+
+router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+
 
 export default router
