3af7474eb44136b8bc17d10301f62e0516498f14
Samir Azam <azamsamir190@gmail.com>
2025-07-24 16:36:43 +0530

    feat: finished real-time language exchange app with chat and video calling functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,13 @@ import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 dotenv.config()
 import cors from 'cors'
+
+import path from 'path'
 const app = express()
 
 const port = process.env.PORT
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -22,6 +26,14 @@ import chatRoute from './routes/chat.route.js'
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/chat",chatRoute)
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*",(req, res)=>{
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+  })
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
